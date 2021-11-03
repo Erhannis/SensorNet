2ae0fa277244530364704a415bf160dbe081bf81
erhannis <eyeillus@gmail.com>
2021-11-02 20:50:51 -0400

    Added pin reading, and reports

--- SensorNet.ino
+++ SensorNet.ino
@@ -14,12 +14,17 @@
 
 class Prefs {
   public:
+  long overseerId;
   String name;
+  int sensor; // -1 disabled, -2 analog, -3 I2C, else GPIO pin
+  int interval; // milliseconds
 };
 
 bool writePrefString(String key, String value, bool mount = true);
+bool writePrefInt(String key, int value, bool mount = true);
 
 bool fsErr = false;
+long nextTime = 0;
 
 Scheduler userScheduler; // to control your personal task
 Prefs prefs;
@@ -30,24 +35,11 @@ void sendMessage() ; // Prototype so PlatformIO doesn't complain
 
 Task taskSendMessage( TASK_MILLISECOND * 500 , TASK_FOREVER, &sendMessage );
 
-long overseerId = -1;
-
-/*
- * Commands:
- * O:... - Set overseer id
- * N:... - Set name
- * 
- * Reports:
- * D:... - Data
- * B:... - Button
- * E:... - Error
- */
-
 void sendReport(String msg) {
-  if (overseerId == -1) {
+  if (prefs.overseerId == -1) {
     mesh.sendBroadcast(msg);
   } else {
-    mesh.sendSingle((uint32_t)overseerId, msg);
+    mesh.sendSingle((uint32_t)prefs.overseerId, msg);
   }
 }
 
@@ -60,20 +52,6 @@ void sendMessage() {
   String msg = "MY NAME, IS, " + prefs.name;
   mesh.sendBroadcast( msg );
   //taskSendMessage.setInterval( random( TASK_SECOND * 1, TASK_SECOND * 5 ));
-}
-
-// Needed for painless library
-void receivedCallback( uint32_t from, String &msg ) {
-  Serial.printf("startHere: Received from %u msg=%s\n", from, msg.c_str());
-  switch (msg[0]) {
-    case 'O': // Overseer id
-      overseerId = msg.substring(2).toInt();
-      break;
-    case 'N': // Set name
-      prefs.name = msg.substring(2);
-      writePrefString("name", prefs.name);
-      break;
-  }
 }
 
 void newConnectionCallback(uint32_t nodeId) {
@@ -117,12 +95,41 @@ bool writePrefString(String key, String value, bool mount) {
   return true;
 }
 
+bool writePrefInt(String key, int value, bool mount) {
+  if (mount) {
+    if (!SPIFFS.begin()) {
+      Serial.println("An error has occurred while mounting SPIFFS");
+      fsErr = true;
+      return false;
+    } else {
+      Serial.println("Successfully mounted SPIFFS");
+    }
+  }
+
+  String path = "/prefs/"+key;
+  
+  File f = SPIFFS.open(path,"w");
+  if (f) {
+    f.print(String(value));
+    f.close();
+    Serial.println("Created "+path);
+  } else {
+    Serial.println("Failed to create "+path);
+    return false;
+  }
+
+  if (mount) {
+    SPIFFS.end();
+  }
+  return true;
+}
+
 String readPrefString(String key, String def, bool mount = true) {
   if (mount) {
     if (!SPIFFS.begin()) {
       Serial.println("An error has occurred while mounting SPIFFS");
       fsErr = true;
-      return ""; // ...I guess empty strings just aren't allowed for values :/
+      return ""; //TODO Return def?  // ...I guess empty strings just aren't allowed for values :/
     } else {
       Serial.println("Successfully mounted SPIFFS");
     }
@@ -134,7 +141,7 @@ String readPrefString(String key, String def, bool mount = true) {
     if (writePrefString(key, def, mount)) {
       return def;
     } else {
-      return "";
+      return ""; //TODO Return def?
     }
   }
 
@@ -148,7 +155,47 @@ String readPrefString(String key, String def, bool mount = true) {
     f.close();
   } else {
     Serial.println("Failed to open " + path);
-    return "";
+    return ""; //TODO Return def?
+  }
+ 
+  if (mount) {
+    SPIFFS.end();
+  }
+  return result;
+}
+
+int readPrefInt(String key, int def, bool mount = true) {
+  if (mount) {
+    if (!SPIFFS.begin()) {
+      Serial.println("An error has occurred while mounting SPIFFS");
+      fsErr = true;
+      return -1; //TODO Return default?
+    } else {
+      Serial.println("Successfully mounted SPIFFS");
+    }
+  }
+
+  String path = "/prefs/"+key;
+
+  if (!SPIFFS.exists(path)) {
+    if (writePrefInt(key, def, mount)) { //TODO Write as binary?
+      return def;
+    } else {
+      return -1; //TODO Return def?
+    }
+  }
+
+  int result = -1;
+  
+  File f = SPIFFS.open(path,"r");
+  if (f) {
+    Serial.print("Opened "+path+": ");
+    result = f.readString().toInt();
+    Serial.print(result);
+    f.close();
+  } else {
+    Serial.println("Failed to open " + path);
+    return -1; //TODO Return def?
   }
  
   if (mount) {
@@ -167,16 +214,113 @@ bool loadPrefs() {
   }
 
   prefs.name = readPrefString("name", "UNNAMED", false);
+  prefs.overseerId = readPrefInt("overseer", -1, false);
+  prefs.sensor = readPrefInt("sensor", 5, false);
+  prefs.interval = readPrefInt("interval", 1000, false);
   //readPref("", "UNNAMED", false);
 
   SPIFFS.end();
   return true;
 }
 
+/*
+ * Commands:
+ * O:... - Set overseer id
+ * N:... - Set name
+ X L:(X|I|A|#) - Enable sensor ('listen'):
+ *   X - disable
+ X   I - I2C
+ *   A - Analog input
+ *   # - GPIO pin
+ * I:... - Set interval
+ * Q:(O|N|L|I) - Request setting
+ X W - Wipe flash
+ * 
+ * Reports:
+ * D:... - Data
+ * B:... - Button
+ * E:... - Error
+ * R:... - Request response
+ */
+
+
+// Needed for painless library
+void receivedCallback( uint32_t from, String &msg ) {
+  Serial.printf("startHere: Received from %u msg=%s\n", from, msg.c_str());
+  switch (msg[0]) {
+    case 'O': // Overseer id
+      prefs.overseerId = msg.substring(2).toInt();
+      writePrefInt("overseer", prefs.overseerId);
+      break;
+    case 'N': // Set name
+      prefs.name = msg.substring(2);
+      writePrefString("name", prefs.name);
+      break;
+    case 'L': // Set sensor
+      switch (msg[2]) {
+        case 'X': // Disable
+          prefs.sensor = -1;
+          break;
+        case 'I': // I2C
+          prefs.sensor = -3;
+          break;
+        case 'A': // Analog
+          prefs.sensor = -2;
+          break;
+        default: // GPIO pin
+          prefs.sensor = msg.substring(2).toInt();
+          pinMode(prefs.sensor, INPUT); // Pull low?
+          break;
+      }
+      writePrefInt("sensor", prefs.sensor);
+      break;
+    case 'I': { // Set interval
+      long t = nextTime - prefs.interval;
+      prefs.interval = msg.substring(2).toInt();
+      nextTime = t + prefs.interval;
+      writePrefInt("interval", prefs.interval);
+      break;}
+    case 'Q': { // Request setting
+      String s = "R:";
+      switch (msg[2]) {
+        case 'O':
+          sendReport(s + prefs.overseerId);
+          break;
+        case 'N':
+          sendReport(s + prefs.name);
+          break;
+        case 'L':
+          switch (prefs.sensor) {
+            case -1: // Disabled
+              sendReport("R:X");
+              break;
+            case -2: // Analog
+              sendReport("R:A");
+              break;
+            case -3: // I2C
+              sendReport("R:I");
+              break;
+            default: // GPIO
+              sendReport(s + prefs.sensor);
+              break;
+          }
+          break;
+        case 'I':
+          sendReport(s + prefs.interval);
+          break;
+      }
+      break;}
+  }
+}
+
 void setup() {
   Serial.begin(115200);
 
+  pinMode(0, INPUT); // Pull low?
   loadPrefs(); // Should handle failure?
+  if (prefs.sensor >= 0) {
+    pinMode(prefs.sensor, INPUT); // Pull low?
+  }
   
   mesh.setDebugMsgTypes( ERROR | MESH_STATUS | CONNECTION | SYNC | COMMUNICATION | GENERAL | MSG_TYPES | REMOTE ); // all types on
 //mesh.setDebugMsgTypes( ERROR | STARTUP );  // set before init() so that you can see startup messages
@@ -192,6 +336,7 @@ void setup() {
 }
 
 int lastBtn = 1;
+//int lastValue = -1;
 void loop() {
   int curBtn = digitalRead(0);
   if (lastBtn != curBtn) {
@@ -202,6 +347,35 @@ void loop() {
     }
   }
   lastBtn = curBtn;
+
+  long t = millis();
+  if (t >= nextTime) {
+    String s;
+    int val;
+    switch (prefs.sensor) {
+      case -1: // Disabled
+        break;
+      case -2: // Analog
+        s = "D:";
+        val = analogRead(A0);
+        sendReport(s + val);
+        break;
+      case -3: // I2C
+        //TODO Do
+        sendReport("E:I2C not yet implemented");
+        break;
+      default: // GPIO
+        val =  digitalRead(prefs.sensor);
+        //TODO If change?
+        if (val) {
+          sendReport("D:1");
+        } else {
+          sendReport("D:0");
+        }
+        break;
+    }
+    nextTime = t + prefs.interval;
+  }
   
   // it will run the user scheduler as well
   mesh.update();
